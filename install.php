88516ca39638d9ed303d0a0ed1c9847884f855d5
@TobiazJS <tobiasjaya3@gmail.com>
2020-05-09 20:05:02 +0700

    api key custom-np

--- install.php
+++ install.php
@@ -2,12 +2,12 @@
 
 // Set variables for our request
 $shop = $_GET['shop'];
-$api_key = "6bde10a78cce92542b9ae7916dae5b7e";
+$api_key = "b111c407af10893f719dc0b0c83a5d30";
 $scopes = "read_orders,write_products";
-$redirect_uri = "https://localhost/shopifyapp/generate_token.php";
+$redirect_uri = "https://zubirr.xyz/installShopify/generate_token.php";
 
 // Build install/approval URL to redirect to
-$install_url = "https://naked-press.myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
+$install_url = "https://np2019.myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
 
 // Redirect
 header("Location: " . $install_url);
